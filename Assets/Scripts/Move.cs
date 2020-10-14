01a9779b072485876ba98a2b67b62f8dfb13bc1e
Hiwaki Kosuke <dareka0530@yahoo.co.jp>
2020-10-14 20:18:40 +0900

    uwaa

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,24 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
 
 public class Move : MonoBehaviour
 {
-    Transform root,parent;
+    [SerializeField]Transform root,parent;
+    public float startposition;
+    public GameObject uma;
     public float speed,rotateSpeed;
     // Start is called before the first frame update
     void Start()
     {
         root = transform.root;
         parent = transform.parent;
+        Invoke("Setup", 0.1f);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         var vel = root.transform.forward*Time.deltaTime*speed;
-        var rot = Quaternion.Euler(new Vector3(0,parent.localPosition.x*rotateSpeed, 0));
+        var rot = Quaternion.Euler(new Vector3(0,(parent.localPosition.x - startposition)*rotateSpeed, 0));
         root.position = root.position + vel;
-        root.rotation = rot * root.rotation;
+        //root.rotation = rot * root.rotation;
+        root.RotateAround(uma.transform.position, Vector3.up, (parent.localPosition.x - startposition)*rotateSpeed);
+        //uma.transform.rotation = rot * uma.transform.rotation;
+    }
+
+    void ResetPosition(){
+        uma.transform.position = transform.position - Vector3.up;
+    }
+
+    void Setup(){
+        startposition = parent.localPosition.x;
+        ResetPosition();
     }
 }
