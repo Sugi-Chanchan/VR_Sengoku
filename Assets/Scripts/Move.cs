94eb1c58f052ce95fdc9be299c3746f027c5af86
杉野将史 <sgnmssuioaai@gmail.com>
2020-10-15 18:18:21 +0900

    手綱の一部実装

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -21,10 +21,11 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        var vel = root.transform.forward*Time.deltaTime*speed;
-        var rot = Quaternion.Euler(new Vector3(0,(parent.localPosition.x - startposition)*rotateSpeed, 0));
-        root.position = root.position + vel;
-        root.rotation = rot * root.rotation;
+        //var vel = root.transform.forward*Time.deltaTime*speed;
+        //var rot = Quaternion.Euler(new Vector3(0,(parent.localPosition.x - startposition)*rotateSpeed, 0));
+        //root.position = root.position + vel;
+        //root.rotation = rot * root.rotation;
+
         //root.RotateAround(uma.transform.position, Vector3.up, (parent.localPosition.x - startposition)*rotateSpeed);
         //uma.transform.rotation = rot * uma.transform.rotation;
     }
@@ -35,7 +36,7 @@ public class Move : MonoBehaviour
 
     void Setup(){
         startposition = parent.localPosition.x;
-        ResetPosition();
+       // ResetPosition();
         Vector3 VRTKposition = VRTK.position;
         root.position = new Vector3(transform.position.x, root.position.y, transform.position.z);
         VRTK.position = VRTKposition;
---
+++ Assets/Scripts/Reins.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Reins : MonoBehaviour
+{
+    
+    VRTK_InteractableObject interactableObject;
+    Vector3 startPosition;
+    Transform root;
+    [SerializeField] float rotateSpeed,speed,maxSpeed;
+    VRTK.VRTK_VelocityEstimator leftHand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        root = transform.root;
+        Invoke("SetUp", 0.2f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (interactableObject.IsGrabbed())
+        {
+            Grabbed();
+        }
+        else
+        {
+            Idle();
+        }
+
+        var vel = root.transform.forward * Time.deltaTime * speed;
+        root.position = root.position + vel;
+    }
+
+    void SetUp()
+    {
+        interactableObject = GetComponent<VRTK_InteractableObject>();
+        startPosition = transform.localPosition;
+        leftHand = GameObject.FindGameObjectWithTag("LeftHand").transform.parent.GetComponent<VRTK_VelocityEstimator>();
+    }
+
+    void Grabbed()
+    {
+        var reinsPosition = transform.localPosition - startPosition;
+
+        if (false) { //腕を振ったかを判定
+        }
+        Rotate(reinsPosition.x);
+        Deceleration(reinsPosition.z);
+    }
+
+   
+
+
+    void Idle()
+    {
+        transform.localPosition = startPosition;
+    }
+
+    void Rotate(float reinPosX)
+    {
+
+        var rot = Quaternion.Euler(new Vector3(0,reinPosX*rotateSpeed, 0));
+        root.rotation = rot * root.rotation;
+    }
+
+    void Deceleration(float reinPosZ)
+    {
+        if (reinPosZ < -0.27f)
+        {
+            speed += reinPosZ * Time.deltaTime;
+            speed = (speed < 0) ? 0 : speed;
+        }
+    }
+    
+    void Acceleration()
+    {
+
+    }
+}
