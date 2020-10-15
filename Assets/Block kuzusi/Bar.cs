2ba18f94fbcb90a63bca086bbc17e02f00eb4776
Hiwaki Kosuke <dareka0530@yahoo.co.jp>
2020-10-15 18:16:01 +0900

    Resetを3秒間トリガー押したらという条件に変更

---
+++ Assets/Block kuzusi/Bar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform BarTrans = GameObject.Find("Bar").transform;
+        Vector3 pos = BarTrans.position;
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            if(pos.x < 20)
+            {
+                this.transform.Translate(0.5f, 0f, 0f);
+            }
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            if(pos.x > -20)
+            {
+                this.transform.Translate(-0.5f, 0f, 0f);
+            }
+        }
+    }
+}
