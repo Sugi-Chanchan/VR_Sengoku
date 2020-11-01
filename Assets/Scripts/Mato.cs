1ed01183ec94a55d71c2b720e8cdb5e5a7d58498
Hiwaki Kosuke <dareka0530@yahoo.co.jp>
2020-11-01 17:38:35 +0900

    矢をつがえた状態で新たに矢を掴むと撃てなくなるバグを修正、Arrowが的のColliderに当たった時に落ちる用に設定

---
+++ Assets/Scripts/Mato.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mato : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.name == "Arrow")
+        {
+            collision.transform.parent = this.transform.GetChild(0);
+            this.transform.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+}
