a8de53fd29e7746584bf8e12ee12cef9da86d306
Hiwaki Kosuke <dareka0530@yahoo.co.jp>
2020-12-14 17:04:02 +0900

    両手持ち

--- Assets/Scripts/Tatamigiri.cs
+++ Assets/Scripts/Tatamigiri.cs
@@ -22,12 +22,15 @@ public class Tatamigiri : StickCollider
         Polygon polygon = colliderInfo.hitPolygon;
         //Vector3 normal = Vector3.Cross(polygon.vertices[1] - polygon.vertices[0], polygon.vertices[2] - polygon.vertices[0]);
         Vector3 normal = polygon.normal;
-        if(normal.y > 0)
+        if(normal.y < 0)
         {
             normal *= -1;
         }
 
-        GameObject[] fragments = MeshCut.CutMesh(this.gameObject, colliderInfo.hitPoints[0], normal, true, cutSurfaceMaterial);
-        fragments[1].AddComponent<Rigidbody>();
+        (GameObject fragmentsCopy, GameObject fragmentsOriginal) = MeshCut.CutMesh(this.gameObject, colliderInfo.hitPoints[0], normal, true, cutSurfaceMaterial);
+        if(fragmentsCopy != null && fragmentsCopy.GetComponent<Rigidbody>() == null)
+        {
+            fragmentsCopy.AddComponent<Rigidbody>();
+        }
     }
 }
