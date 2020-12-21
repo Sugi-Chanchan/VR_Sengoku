06bcdc30b18e731bd2de70fe7dac385ef6d36144
Hiwaki Kosuke <dareka0530@yahoo.co.jp>
2020-12-21 17:42:02 +0900

    刀の追加

---
+++ Assets/Scripts/Windbreak.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Windbreak : MonoBehaviour
+{
+    [SerializeField] Rigidbody weaponRigidbody;
+    [SerializeField] AudioSource weaponAudio;
+    [SerializeField] AudioClip windbreakSound;
+    float speed;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        speed = weaponRigidbody.velocity.magnitude;
+        //print(speed);
+        if(speed > 0.1)
+        {
+            print("うおお");
+            //audio.PlayOneShot(windbreakSound);
+            weaponAudio.PlayOneShot(windbreakSound);
+        }
+    }
+}
