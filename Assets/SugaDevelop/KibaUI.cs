dd19bd7b89f2532802163c85f078b59b9b8ed819
杉野将史 <sgnmssuioaai@gmail.com>
2021-01-12 18:53:22 +0900

    HP表示

---
+++ Assets/SugaDevelop/KibaUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KibaUI : MonoBehaviour
+{
+    static KibaUI kibaUI;
+
+    public static KibaUI ui
+    {
+        get
+        {
+            return kibaUI; 
+        }
+    }
+
+
+    private void Awake()
+    {
+        kibaUI = this;
+    }
+
+    public Text hpText;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    public void SetHp(int hp)
+    {
+        hpText.text = "HP:" + hp;
+    }
+
+}
--- Assets/SugaDevelop/PlayerBody.cs
+++ Assets/SugaDevelop/PlayerBody.cs
@@ -1,17 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading.Tasks;
+using System;
 
 public class PlayerBody : StickColliderDynamic
 {
-    
-
+    int hp = 10;
+    bool hitted = false;
     public override void OnCollision(CollisionInfo collisionInfo)
     {
         if (collisionInfo.collisionObject.tag == "EnemyWeapon")
         {
-
-            print("You Lose!");
+            if (hitted) { return; }
+            hitted = true;
+            hp -= 1;
+            KibaUI.ui.SetHp(hp);
+            WaitForAsynic(2, () => hitted = false);
         }
     }
+
+    private async void WaitForAsynic(int seconds,Action action)
+    {
+        await Task.Delay(TimeSpan.FromSeconds(seconds));
+        action();
+    }
+    
 }
